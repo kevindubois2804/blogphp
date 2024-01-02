d8d413c13d7e5dc7ded05557037b1a5c286f4c32
duboiskevin2804@gmail.com <duboiskevin2804@gmail.com>
2024-01-02 16:21:57 +0100

    fixed inscription page

--- www/inscription.php
+++ www/inscription.php
@@ -1,19 +1,26 @@
 <?php require(__DIR__ . DIRECTORY_SEPARATOR . "inc" . DIRECTORY_SEPARATOR . "init.php") ?>
 <?php
+
 if ($_POST) {
-    // debug($_POST['email']);
-    $utilisateur = executeRequete("select * from utilisateur where slug='$_POST[pseudo]'");
-    if ($utilisateur->num_rows > 0) {
-        $contenu .= "<div class='erreur'>Pseudo indisponible. Veuillez en choisir un autre svp.</div>";
-    } else {
-        $_POST['passwrd'] = password_hash($_POST['passwrd'], PASSWORD_BCRYPT);
-        executeRequete("INSERT INTO blog.utilisateur
-        (id_user, slug, email, passwrd, is_admin)
-        VALUES(0, '$_POST[pseudo]', '$_POST[email]', '$_POST[passwrd]', 0);");
-        $contenu .= "<div class='validation'>Vous êtes inscrit à notre site web. <a href=\"connexion.php\"><u>Cliquez ici pour vous connecter</u></a></div>";
-        sleep(3);
-        header("location: connexion.php");
-    }
+  // debug($_POST);
+  $verif_caractere = preg_match('#^[a-zA-Z0-9._-]+$#', $_POST['slug']);
+  if (!$verif_caractere || (strlen($_POST['slug']) < 1 || strlen($_POST['slug']) > 20)) // 
+  {
+      $contenu .= "<div class='erreur'>Le pseudo doit contenir entre 1 et 20 caractères. <br> Caractère accepté : Lettre de A à Z et chiffre de 0 à 9</div>";
+  } else {
+      $utilisateur = executeRequete("SELECT * FROM utilisateur WHERE slug='$_POST[slug]'");
+      if ($utilisateur->num_rows > 0) {
+          $contenu .= "<div class='erreur'>Pseudo indisponible. Veuillez en choisir un autre svp.</div>";
+      } else {
+          $_POST['passwrd'] = password_hash($_POST['passwrd'], PASSWORD_BCRYPT);
+          foreach ($_POST as $indice => $valeur) {
+              $_POST[$indice] = htmlEntities(addSlashes($valeur));
+          }
+          executeRequete("INSERT INTO utilisateur (slug, email, passwrd, is_admin) VALUES ('$_POST[slug]', '$_POST[email]', '$_POST[passwrd]', 0)");
+          // $contenu .= "<div class='validation'>Vous êtes inscrit à notre site web. <a href=\"connexion.php\"><u>Cliquez ici pour vous connecter</u></a></div>";
+          header('location: connexion.php');
+      }
+  }
 }
 
 ?>
